8bb80b2ef6d309daf29070fc8f26d9a1dd2c2753
Ivan Korhonen <94924152+korvanen@users.noreply.github.com>
2026-03-07 11:38:53 +0200

    Update utils-concept-map.js

--- utilities/utils-concept-map.js
+++ utilities/utils-concept-map.js
@@ -673,7 +673,7 @@ function initConceptMapTool(paneEl, sidebarEl) {
              || CMAP_FALLBACK_PALETTE;
 
     if (pal && pal.length >= 5) {
-      const rotated = [pal[4], pal[0], pal[1], pal[2], pal[3], pal[4], pal[5] || pal[0]];
+      const rotated = [pal[0], pal[1], pal[2], pal[3], pal[4], pal[5], pal[0] || pal[1]];
       return rotated[Math.min(idx, rotated.length - 1)];
     }
 
